e646fcdaec2d59ea9b13823108b1c6a25bb64173
ainibaoer2014 <756367019@qq.com>
2017-06-29 17:25:18 +0800

    更新文章

--- routes/article.js
+++ routes/article.js
@@ -3,7 +3,7 @@ let {checkLogin,checkNotLogin}=require('../auth');
 let {Article}=require('../model');
 let router=express.Router();
 router.get('/add',checkLogin,function (req,res) {
-    res.render('article/add',{title:"发表文章"});
+    res.render('article/add',{title:"发表文章",article:{}});
 });
 router.post('/add',checkLogin,function (req,res) {
     let article=req.body;
@@ -43,6 +43,32 @@ router.get('/delete/:_id',function (req,res) {
         }else{
             req.flash('success','删除文章成功！');
             res.redirect('/');
+        }
+    });
+});
+
+//更新/编辑文章路由
+router.get('/update/:_id',function (req,res) {
+    let _id=req.params._id;
+    Article.findById(_id,function (err,article) {
+        //增加/更新可以复用一个模板
+        res.render('article/add',{title:'更新文章',article});
+    });
+});
+
+//编辑/更新文章后提交的路由
+router.post('/update/:_id',function (req,res){
+    let _id=req.params._id;
+    let article=req.body;
+    //虽然更新时，只有两个字段，增加文章时有四个字段，但是，它只会更新同名属性，不会全部覆盖整个对象
+    Article.update({_id},article,function (err,result) {
+        if(err){
+            req.flash('error',err);
+            res.redirect('back');
+        }else{
+            req.flash('success','文章更新成功!');
+            //更新成功后，跳到详情页，如果跳到首页，由于文章太多查找不方便
+            res.redirect('/article/detail/'+_id);
         }
     });
 });
